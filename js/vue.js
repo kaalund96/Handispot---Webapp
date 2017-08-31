e260ffdc5360431e51c33dc8ad2e1a29fac4f407
BoletteBastrup <31208305+BoletteBastrup@users.noreply.github.com>
2017-08-31 12:37:40 +0200

    Add files via upload

---
+++ js/vue.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        items: [{
+                address: 'Brunsgalleri',
+                distance: "2,5"
+            },
+            { address: 'ftgftfg' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+            { address: 'Brunsgalleri' },
+        ]
+    }
+});
