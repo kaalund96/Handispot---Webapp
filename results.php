f9b7a0e56888c4e8b7a8d5b097323bccea3440aa
BoletteBastrup <31208305+BoletteBastrup@users.noreply.github.com>
2017-08-31 12:38:32 +0200

    list

--- results.php
+++ results.php
@@ -1,18 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="https://unpkg.com/vue@2.4.2"></script>
+
+</head>
+
+<body>
     <header>
         <img src="">
         <a href="search.php">Search</a>
     </header>
     <main>
         <h1>15 results found</h1>
+        <div id="app">
+            <ul>
+                <li v-for="item in items">
+                    <img id="ptag" src="images/p.png">
+                    <h3>{{item.address}}</h3>
+                    <h4>({{item.distance}}km væk)</h4>
+
+                </li>
+            </ul>
+        </div>
     </main>
-    </body>
+</body>
+<script src="vue.js"></script>
+
 </html>
