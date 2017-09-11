7818fd84d67c9075ade0212f5e5265d20f2ebe79
kaalund96 <31208070+kaalund96@users.noreply.github.com>
2017-09-11 08:56:44 +0200

    small animation

--- Handispot-2/search.php
+++ Handispot-2/search.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/animate.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
@@ -38,7 +39,7 @@
 
     </ul>
     <footer>
-        <button id="nSearch">Search</button>
+        <button id="nSearch" class="animated zoomIn">Search</button>
     </footer>
 
 
